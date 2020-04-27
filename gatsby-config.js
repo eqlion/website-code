05e9bd80f38626e3580f594abbbe6d8042f72b20
Nikolay Lapa <lapanicholas@gmail.com>
2020-04-27 22:00:03 +0300

    fix just broken exports //dumbass

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,4 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-export const plugins = [`gatsby-plugin-favicon`];
+module.exports = {
+    /* Your site config here */
+    plugins: [`gatsby-plugin-favicon`],
+};
